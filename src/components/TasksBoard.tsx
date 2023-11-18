c08891127d56f89e9f92f27ae559800d46f9676a
Brayan <brayanruiz11111@gmail.com>
2023-11-17 23:29:26 -0500

    change

--- src/components/TasksBoard.tsx
+++ src/components/TasksBoard.tsx
@@ -1,37 +1,62 @@
-import React from 'react'
-import { Task, TaskStatus } from '../Types';
-
+import React from "react";
+import { Task, TaskStatus } from "../Types";
+import { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.css';
 
 const TasksBoard = () => {
-	// a. funcionalidad con react
-	// b. diseño de ui
+  // a. funcionalidad con react
+  // b. diseño de ui
 
-	// montar un estado
-	// dos estados
-	// 1. lista de tareas
-	// 2. creacion de la tarea
-	const [list, setList] = React.useState<Task[]>([]);
-	const [newTask, setNewTask] = React.useState<Task>({
-		status: TaskStatus.TODO,
-		title: ''
-	});
+  // montar un estado
+  // dos estados
+  // 1. lista de tareas
+  // 2. creacion de la tarea
+  const [list, setList] = useState<Task[]>([]);
+  const [newTask, setNewTask] = useState<Task>({
+    status: TaskStatus.TODO,
+    title: "",
+  });
 
 
-	// 3. ui para agregar una tarea
-	// 4. change handler para el input name
-	// 5. click handler para el boton - agregar tarea -> list
-	return (
-		<>
-			<div>TasksBoard</div>
-			<input
-			style={{width:300, height: 25  }}  
-			type="text" name="title" value={newTask.title} placeholder='Introduzca un titulo para la tarea' />
-			<button
-			type='button'
-			onClick={() => setNewTask(newTask)}>Añadir tarea</button>
-			<button>Editar tarea</button>
-		</>
-	)
-}
+  const handleInputChange = (e:any) =>{
+	console.log(e.target.value);
+	setNewTask({
+		...newTask,
+		[e.target.name] : e.target.value
+	})
+  }
 
-export default TasksBoard
+  const addList = (e:any) =>{
+	e.preventDefault();
+	}
+
+  // 3. ui para agregar una tarea
+  // 4. change handler para el input name
+  // 5. click handler para el boton - agregar tarea -> list
+  return (
+    <>
+      <div>TasksBoard</div>
+	  <form
+	  
+	  className="mb-3">
+	  <input
+        className="form-label"
+        type="text"
+        name="title"
+        placeholder="Introduzca un titulo para la tarea"
+		onClick={handleInputChange}
+      />
+      <button
+	  	onClick={addList}
+	    className="btn btn-primary" 
+		type="button" >
+        Añadir tarea
+      </button>
+    
+	  </form>
+     
+    </>
+  );
+};
+
+export default TasksBoard;
