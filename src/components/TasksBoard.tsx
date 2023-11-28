29b8eb55a83002571ecf4b9bd07ada91297381f9
Brayan <brayanruiz11111@gmail.com>
2023-11-27 22:27:28 -0500

    was added function edit and delete

--- src/components/TasksBoard.tsx
+++ src/components/TasksBoard.tsx
@@ -1,14 +1,16 @@
 import "bootstrap/dist/css/bootstrap.css";
 import { useState } from "react";
-import { Task, TaskStatus } from "../Types";
+import { Task, TaskStatus } from '../Types';
 
-const TasksBoard = () => { 
+const TasksBoard = () => {
   const [list, setlist] = useState<Task[]>([]);
+  const [editingTask, setEditingTask] = useState<Task | null>(null);
   const [newTask, setNewTask] = useState<Task>({
     status: TaskStatus.TODO,
     title: "",
-    id: 654653216464,
+    id: "",
   });
+  
 
   // GENERATE ID
   const generateId = () => {
@@ -18,73 +20,89 @@ const TasksBoard = () => {
   };
 
   // ADD TASK
-  const handleSubmit = (e: any) => {
+  const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     if (newTask.title === "") {
       alert("Agregue una tarea");
       return;
     }
 
-    const task: Task = {
-      id: "",
-      title: "",
-      status: TaskStatus.TODO,
-    };
-    const temp = [newTask, ...list];
-    task.id = generateId();
-    setlist([...temp]);
+    if (editingTask) {
+      // If there is a task in edit, update that task instead of adding a new one
+      const updatedList = list.map((task) =>
+        task.id === editingTask.id ? newTask : task
+      );
+      setlist(updatedList);
+      setEditingTask(null);
+    } else {
+      // Add new task
+      newTask.id = generateId();
+      setlist([newTask, ...list]);
+      console.log(list)
+    }
 
-    setNewTask({ status: TaskStatus.TODO, title: "", id: generateId() });
-  };
-
-  // CHANGE
-  const handleInputChange = (e: any) => {
+    // Clear input field after adding/updating task
     setNewTask({
-      ...newTask,
-      [e.target.name]: e.target.value,
+      status: TaskStatus.TODO,
+      title: "",
+      id: "",
     });
   };
 
+  // CHANGE
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTask({
+      ...newTask,
+      title: e.target.value,
+    });
+  };
+
+  // EDIT
+  const handleEdit = (task: Task) => {
+    setNewTask(task);
+    setEditingTask(task);
+  };
+
   // DELETE
-  const handleDelete = (id: any) => {
-    const deleteTask = list.filter((newTask) => newTask.id !== id);
-    setlist(deleteTask);
+  const handleDelete = (id: string) => {
+    const tasksUpdated = list.filter((task) => task.id !== id);
+    setlist(tasksUpdated);
+    setEditingTask(null); // Clear task in edit if deleted
   };
 
   return (
     <>
       <div>TasksBoard</div>
-      <form className="mb-3">
+      <form className="mb-3" onSubmit={handleSubmit}>
         <input
           className="form-label"
           type="text"
           name="title"
           value={newTask.title}
-          placeholder="Introduzca la tarea"
+          placeholder="Enter a task"
           onChange={handleInputChange}
         />
-        <button
-          onClick={handleSubmit}
-          className="btn btn-primary"
-          type="submit"
-        >
-          Añadir tarea
+        <button className="btn btn-primary" type="submit">
+          {editingTask ? "Update Task" : "Add New Task"}
         </button>
       </form>
 
       <div>
         Tasks List
-        {list.map((Task: any) => (
-          <li key={Task.id}>
-            <h2>{Task.title}</h2>
-            <button className="btn btn-primary">Edit</button>
+        {list.map((task:any) => (
+          <li key={task.id}>
+            <h2>{task.title}</h2>
             <button
               className="btn btn-primary"
-              onClick={() => {
-                handleDelete(newTask.id);
-              }}
+              onClick={() => handleEdit(task)}
             >
-              delete
+              Edit
+            </button>
+            <button
+              className="btn btn-primary"
+              onClick={() => handleDelete((task.id))}
+            >
+              Delete
             </button>
           </li>
         ))}
